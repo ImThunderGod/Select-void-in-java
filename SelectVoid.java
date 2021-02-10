ba9ecd62e5a03ac8b9dd565d4a20d8e1e74c8d43
ANAND DASH <76928212+ImThunderGod@users.noreply.github.com>
2021-02-10 21:53:26 +0530

    Add files via upload

---
+++ SelectVoid.java
@@ -0,0 +1,12 @@
+
+package com.company;
+
+public class SelectVoid {
+    static int myMethod(int x) {
+        return 5 + x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(6));
+    }
+}
